3ed901c4c8a53a59cbc16d4a6d05d7ebdfbbe063
afndy <moamenmahmoud118@gmail.com>
2022-10-29 01:59:30 +0200

    moamen

--- app/src/main/java/com/example/myapplication6/LoginActivity.java
+++ app/src/main/java/com/example/myapplication6/LoginActivity.java
@@ -5,23 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView btn;
+    EditText inputEmail, inputpassword;
+    Button btnlogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView btn=findViewById(R.id.textViewSignUp);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn = findViewById(R.id.textViewSignUp);
+        inputEmail = findViewById(R.id.editTextTextPersonName3);
+        inputpassword = findViewById(R.id.editTextTextPersonName);
+        btnlogin = findViewById(R.id.button2);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this,regsiter.class));
-
+                chekcrededentials();
             }
         });
 
+
+        btn.setOnClickListener((v) -> {
+            startActivity(new Intent(LoginActivity.this, regsiter.class));
+
+
+        });
+    }
+
+    private void chekcrededentials() {
+        String email=inputEmail.getText().toString();
+        String password=inputpassword.getText().toString();
+
+
+         if (email.isEmpty() || !email.contains("@"))
+        {
+            showError(inputEmail,"البريد الاكتروني خطأ");
+        }
+        else if (password.isEmpty() || password.length()<7)
+        {
+            showError(inputpassword,"كلمة السر اقبل من 7 حروف");
+        }
+        else
+        {
+            Toast.makeText(this, "Call login Method", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }
--- app/src/main/java/com/example/myapplication6/regsiter.java
+++ app/src/main/java/com/example/myapplication6/regsiter.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.ClosedSubscriberGroupInfo;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.internal.RegisterListenerMethod;
+import com.google.android.gms.common.api.internal.RegistrationMethods;
+
 public class regsiter extends AppCompatActivity {
     TextView btn;
 
-    private EditText inputusername, inputpassword, InputEmail, Inputconformpassword;
+    private EditText inputusername, inputpassword, inputEmail, inputconformpassword;
     Button btnregsiter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,66 +26,62 @@ public class regsiter extends AppCompatActivity {
 
         btn = findViewById(R.id.textView2);
         inputusername = findViewById(R.id.editTextTextPersonName);
-        InputEmail = findViewById(R.id.editTextTextPersonName3);
+        inputEmail = findViewById(R.id.editTextTextPersonName3);
         inputpassword = findViewById(R.id.editTextTextPersonName2);
-        Inputconformpassword = findViewById(R.id.editTextTextPersonName4);
+        inputconformpassword = findViewById(R.id.editTextTextPersonName4);
 
         btnregsiter=findViewById(R.id.button);
         btnregsiter.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onclick() {
-            checkCredenrials();
-        }
-
-
-
-
-
-        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(regsiter.this, LoginActivity.class));
-
-
+            public void onClick(View V) {
+                chekcrededentials();
             }
+        });
+        chekcrededentials();
+
+
+        btn.setOnClickListener((v) -> {
+            ;
+            startActivity(new Intent(regsiter.this,LoginActivity. class));
+
+
         });
     }
 
-    private void checkCredenrials() {
-        String username = inputusername.getText().toString();
-        String email = InputEmail.getText().toString();
-        String password = inputpassword.getText().toString();
-        String conformpassword = Inputconformpassword.getText().toString();
 
-        if (username.isEmpty() || username.length() < 7);
-            {
-                showError(inputusername, "اسم المستخدم خطأ");
-            }
-            else if  (email.isEmpty() || email.contains("@")) ;
+    private void chekcrededentials() {
+        String username=inputusername.getText().toString();
+        String email=inputEmail.getText().toString();
+        String password=inputpassword.getText().toString();
+        String conformpassword=inputconformpassword.getText().toString();
 
+        if (username.isEmpty() || username.length()<7)
+        {
+           showError(inputusername,"اسم المستخدم خطأ");
+        }
+        else if (email.isEmpty() || !email.contains("@"))
+        {
+            showError(inputEmail,"البريد الاكتروني خطأ");
+        }
+        else if (password.isEmpty() || password.length()<7)
+        {
+            showError(inputpassword,"كلمة السر اقبل من 7 حروف");
+        }
+        else if (conformpassword.isEmpty() || !conformpassword.equals(password))
+        {
+           showError(inputconformpassword,"كلمة المرور غير متاطبقه");
+        }
+        else
+        {
+            Toast.makeText(this, "Call Registration Method", Toast.LENGTH_SHORT).show();
+        }
 
-        {
-            showError(InputEmail, "هناك خطأ في البريد");
-        }
-          if (password.isEmpty()||password.length()<7);
-        {
-            showError(inputpassword, "لا تحتوي كلمة علي 7 احرف");
-        }
-        if (conformpassword.isEmpty() || !conformpassword.equals(password));
-        {
-            showError(Inputconformpassword,"تأكد من كلمة المرور");
-        }
-        else {
-            Toast.makeText(this, "جاري مرجعة البيانات", Toast.LENGTH_SHORT).show();
-        }
 
     }
 
-    private void showError(EditText input,String s) {
-        input.setError(s);
-        input.requestFocus();
-     }
-    }
 
-    
-    
+    private void showError(EditText input, String s) {
+       input.setError(s);
+       input.requestFocus();
+    }
+}
